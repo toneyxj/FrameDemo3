2416f3a8a86008228ad05ed60c6924cea2aea88b
toneyxj <809939691@qq.com>
2019-02-13 10:18:45 +0800

    添加全局activity控制

--- app/src/main/java/com/xj/framedemo3/ObjApp.java
+++ app/src/main/java/com/xj/framedemo3/ObjApp.java
@@ -4,6 +4,7 @@ import android.os.Debug;
 
 import com.xj.mainframe.BaseApplication;
 import com.xj.mainframe.base.InitializeService;
+import com.xj.mainframe.configer.ToastUtils;
 
 /**
  * Created by xj on 2018/10/23.
@@ -16,6 +17,7 @@ public class ObjApp extends BaseApplication {
         super.onCreate();
 //注册提示
         InitializeService.start(this,myInitService.class);
+        ToastUtils.getInstance().initToast(this);
         Debug.stopMethodTracing();
     }
 }
